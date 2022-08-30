c5d4112663cc8ee0a316eb1454117048226ac5fe
Aashishio <98104806+Aashishio@users.noreply.github.com>
2022-08-30 17:00:31 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+function shower(){
+    var x = document.getElementById("password_input");
+    var y = document.getElementById("shower");
+    var z = document.getElementById("hider");
+
+    if (x.type === 'password'){
+        y.style.display = "none";
+        z.style.display = "block";
+        x.type = "text";
+    }
+    else if(x.type === 'text'){
+        y.style.display = "block";
+        z.style.display = "none";
+        x.type = "password";
+    }
+}
